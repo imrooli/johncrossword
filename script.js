8b8e1adcec016aa1ff4243f954dd77f17633173f
immy <ImranK1223@gmail.com>
2023-08-07 17:50:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,78 @@
-// Array containing URLs of puzzle piece images
 const puzzlePieceUrls = [
   "https://i.imgur.com/4oYIM2J.png",
+  "https://i.imgur.com/zODk3CC.png",
+  "https://i.imgur.com/n5DUWlS.png",
+  "https://i.imgur.com/UVmY9cF.png",
+  "https://i.imgur.com/sytQIcj.png",
+  "https://i.imgur.com/qRdjMLv.png",
+  "https://i.imgur.com/Ufw6yy4.png",
+  "https://i.imgur.com/eWJ82eZ.png",
+  "https://i.imgur.com/mhXCzWQ.png",
+  "https://i.imgur.com/X41bp2L.png",
+  "https://i.imgur.com/nvyDWOv.png",
+  "https://i.imgur.com/9PmClqT.png",
+  "https://i.imgur.com/Xv4MzM6.png",
+  "https://i.imgur.com/0soVHsc.png",
+  "https://i.imgur.com/QUCnbcp.png",
+  "https://i.imgur.com/iqErEnr.png",
+  "https://i.imgur.com/IWhSQp3.png",
+  "https://i.imgur.com/5Duhm4y.png",
+  "https://i.imgur.com/pdvJPNa.png",
+  "https://i.imgur.com/JnD04cm.png",
+  "https://i.imgur.com/H2IACyW.png",
+  "https://i.imgur.com/L3649O7.png",
+  "https://i.imgur.com/MtYGuIt.png",
+  "https://i.imgur.com/ELGdn97.png",
+  
   // Add other image links here for the remaining puzzle pieces
 ];
 
-// Create puzzle pieces and add them to the board
 function createPuzzlePieces() {
   const board = document.getElementById("puzzle-board");
 
   puzzlePieceUrls.forEach((url, index) => {
     const piece = document.createElement("div");
     piece.className = "puzzle-piece";
+    piece.setAttribute("draggable", "true");
+    piece.style.backgroundImage = `url(${url})`;
     piece.style.left = Math.random() * 500 + "px"; // Random initial position
     piece.style.top = Math.random() * 300 + "px";
-    piece.innerHTML = `<img src="${url}" alt="Piece ${index + 1}">`;
-
-    // Add event listeners for dragging the puzzle piece
-    piece.addEventListener("mousedown", handleDragStart);
-    piece.addEventListener("mouseup", handleDragEnd);
 
     board.appendChild(piece);
   });
 }
 
-let draggedPiece = null;
+document.addEventListener("DOMContentLoaded", () => {
+  createPuzzlePieces();
 
-function handleDragStart(event) {
-  draggedPiece = event.target;
-  event.target.style.cursor = "grabbing";
-}
+  const puzzlePieces = document.querySelectorAll(".puzzle-piece");
 
-function handleDragEnd(event) {
-  draggedPiece = null;
-  event.target.style.cursor = "grab";
-}
+  let draggedPiece = null;
 
-document.addEventListener("mousemove", handleDrag);
+  puzzlePieces.forEach(piece => {
+    piece.addEventListener("dragstart", handleDragStart);
+    piece.addEventListener("dragend", handleDragEnd);
+  });
 
-function handleDrag(event) {
-  if (draggedPiece) {
-    draggedPiece.style.left = event.pageX - draggedPiece.offsetWidth / 2 + "px";
-    draggedPiece.style.top = event.pageY - draggedPiece.offsetHeight / 2 + "px";
+  function handleDragStart(event) {
+    draggedPiece = event.target;
+    event.dataTransfer.setData("text/plain", ""); // Required for Firefox to enable dragging
   }
-}
 
-// Initialize the puzzle
-createPuzzlePieces();
+  function handleDragEnd(event) {
+    draggedPiece = null;
+  }
+
+  document.addEventListener("dragover", event => event.preventDefault());
+
+  document.addEventListener("drop", event => {
+    event.preventDefault();
+    if (draggedPiece) {
+      const rect = event.target.getBoundingClientRect();
+      const offsetX = event.clientX - rect.left;
+      const offsetY = event.clientY - rect.top;
+      draggedPiece.style.left = offsetX - draggedPiece.offsetWidth / 2 + "px";
+      draggedPiece.style.top = offsetY - draggedPiece.offsetHeight / 2 + "px";
+    }
+  });
+});
