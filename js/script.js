dd9a112d54bc92da627d5a020c80941b32f04c6a
immy <ImranK1223@gmail.com>
2023-08-07 23:19:39 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,28 +2,32 @@ document.addEventListener('DOMContentLoaded', () => {
     const puzzleBoard = document.getElementById('puzzle-board');
     const puzzlePieces = [];
 
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-    }
-
     function addEventListenersToPieces() {
         puzzlePieces.forEach(puzzlePiece => {
-            puzzlePiece.draggable = true;
-            puzzlePiece.addEventListener('dragstart', startDragging);
-            puzzlePiece.addEventListener('dragend', stopDragging);
+            puzzlePiece.addEventListener('mousedown', startDragging);
         });
+        document.addEventListener('mousemove', continueDragging);
+        document.addEventListener('mouseup', stopDragging);
     }
 
     let draggingPiece = null;
+    let offsetX = 0;
+    let offsetY = 0;
 
     function startDragging(event) {
         draggingPiece = event.target;
-        event.dataTransfer.setData('text/plain', ''); // Required for dragging in Firefox
+        const puzzlePieceRect = draggingPiece.getBoundingClientRect();
+        offsetX = event.clientX - puzzlePieceRect.left;
+        offsetY = event.clientY - puzzlePieceRect.top;
         draggingPiece.style.cursor = 'grabbing';
         draggingPiece.style.zIndex = 1;
+        event.preventDefault(); // Prevent selection while dragging
+    }
+
+    function continueDragging(event) {
+        if (!draggingPiece) return;
+        draggingPiece.style.left = (event.clientX - offsetX) + 'px';
+        draggingPiece.style.top = (event.clientY - offsetY) + 'px';
     }
 
     function stopDragging() {
@@ -37,8 +41,17 @@ document.addEventListener('DOMContentLoaded', () => {
     for (let i = 1; i <= 24; i++) {
         const puzzlePiece = document.createElement('div');
         puzzlePiece.classList.add('puzzle-piece');
-        puzzlePiece.style.backgroundImage = `url(images/puzzle_pieces/puzzle_${i}.png)`;
-        puzzlePiece.style.backgroundSize = 'contain';
+        const puzzleImage = new Image();
+        puzzleImage.src = `images/puzzle_pieces/puzzle_${i}.png`;
+        puzzleImage.onload = () => {
+            const originalWidth = puzzleImage.width;
+            const originalHeight = puzzleImage.height;
+            puzzlePiece.style.width = originalWidth + 'px';
+            puzzlePiece.style.height = originalHeight + 'px';
+            puzzlePiece.style.left = Math.random() * (puzzleBoard.offsetWidth - originalWidth) + 'px';
+            puzzlePiece.style.top = Math.random() * (puzzleBoard.offsetHeight - originalHeight) + 'px';
+        };
+        puzzlePiece.appendChild(puzzleImage);
         puzzleBoard.appendChild(puzzlePiece);
         puzzlePieces.push(puzzlePiece);
     }
