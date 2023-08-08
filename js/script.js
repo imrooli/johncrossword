ec71e06401214726a5db365403f021af6d03dd10
immy <ImranK1223@gmail.com>
2023-08-07 21:29:57 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,60 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
   const puzzleContainer = document.getElementById("puzzle-container");
-  const puzzlePieces = [];
 
-  // Function to create a puzzle piece and make it draggable
-  function createPuzzlePiece(id) {
-    const puzzlePiece = document.createElement("img");
-    puzzlePiece.src = `images/puzzle_pieces/puzzle_${id}.png`;
-    puzzlePiece.id = `puzzle-${id}`;
-    puzzlePiece.classList.add("puzzle-piece");
-    puzzlePiece.draggable = true;
+  // Function to handle drag events
+  function handleDragStart(e) {
+    e.dataTransfer.setData("text/plain", e.target.id);
+    e.dataTransfer.dropEffect = "move";
+  }
 
-    puzzlePiece.addEventListener("dragstart", (e) => {
-      e.dataTransfer.setData("text/plain", e.target.id);
-    });
+  function handleDragOver(e) {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "move";
+  }
 
-    puzzlePieces.push(puzzlePiece);
-    puzzleContainer.appendChild(puzzlePiece);
+  function handleDrop(e) {
+    e.preventDefault();
+    const draggedPieceId = e.dataTransfer.getData("text/plain");
+    const draggedPiece = document.getElementById(draggedPieceId);
+    const offsetX = e.clientX - draggedPiece.dataset.clientX;
+    const offsetY = e.clientY - draggedPiece.dataset.clientY;
+    draggedPiece.style.left = `${offsetX}px`;
+    draggedPiece.style.top = `${offsetY}px`;
+    updatePuzzlePositions();
   }
 
   // Function to update the puzzle piece positions
   function updatePuzzlePositions() {
-    puzzlePieces.forEach((piece) => {
+    const puzzlePieces = document.getElementsByClassName("puzzle-piece");
+    for (let i = 0; i < puzzlePieces.length; i++) {
+      const piece = puzzlePieces[i];
       const rect = piece.getBoundingClientRect();
-      piece.dataset.left = rect.left;
-      piece.dataset.top = rect.top;
-    });
-  }
-
-  // Function to handle drag events
-  function handleDragEvents(e) {
-    e.preventDefault();
-
-    const draggedPieceId = e.dataTransfer.getData("text/plain");
-    const draggedPiece = document.getElementById(draggedPieceId);
-    const offsetX = e.clientX - parseInt(draggedPiece.dataset.left);
-    const offsetY = e.clientY - parseInt(draggedPiece.dataset.top);
-
-    function movePiece(e) {
-      draggedPiece.style.left = e.clientX - offsetX + "px";
-      draggedPiece.style.top = e.clientY - offsetY + "px";
+      piece.dataset.clientX = rect.left;
+      piece.dataset.clientY = rect.top;
     }
-
-    function releasePiece() {
-      puzzleContainer.removeEventListener("mousemove", movePiece);
-      puzzleContainer.removeEventListener("mouseup", releasePiece);
-      updatePuzzlePositions();
-    }
-
-    puzzleContainer.addEventListener("mousemove", movePiece);
-    puzzleContainer.addEventListener("mouseup", releasePiece);
   }
 
   // Load 24 puzzle pieces onto the page
   for (let i = 1; i <= 24; i++) {
-    createPuzzlePiece(i);
+    const puzzlePiece = document.createElement("img");
+    puzzlePiece.src = `images/puzzle_pieces/puzzle_${i}.png`;
+    puzzlePiece.id = `puzzle-${i}`;
+    puzzlePiece.classList.add("puzzle-piece");
+    puzzlePiece.style.position = "absolute";
+    puzzlePiece.style.left = "0";
+    puzzlePiece.style.top = "0";
+
+    puzzlePiece.addEventListener("dragstart", handleDragStart);
+    puzzleContainer.appendChild(puzzlePiece);
   }
 
-  puzzleContainer.addEventListener("dragover", handleDragEvents);
+  puzzleContainer.addEventListener("dragover", handleDragOver);
+  puzzleContainer.addEventListener("drop", handleDrop);
 });
